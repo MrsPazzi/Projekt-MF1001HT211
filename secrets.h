032ff7fe1eb59d0da0cc1aee19c4e4c3f02ad34e
MrsPazzi <50967450+MrsPazzi@users.noreply.github.com>
2022-01-17 22:02:34 +0100

    Update secrets.h

--- secrets.h
+++ secrets.h
@@ -1,8 +1,8 @@
 
 
-#define SECRET_SSID "KTH-IoT"
-#define SECRET_PASS "i2clQBy5Mb" 
+#define SECRET_SSID "KTH-IoT" //Nätverkets namn
+#define SECRET_PASS "i2clQBy5Mb" //Läsenordet till nätverket
 
 
-#define SECRET_CH_ID 1572516		
-#define SECRET_WRITE_APIKEY "0PRIC5C7G8E9M352"   
+#define SECRET_CH_ID 1572516		//Kanal id för thingspeak kanalen
+#define SECRET_WRITE_APIKEY "0PRIC5C7G8E9M352"   //Nyckeln för att kunna lägga till värden från sensorerna på thingspeak.
